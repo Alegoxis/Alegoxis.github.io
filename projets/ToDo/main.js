efd9ead236a18a3f304112fd3670b3039445b364
Alegoxis <alexisbrossardguillon@gmail.com>
2025-10-27 17:58:09 +0100

    ToDolist added

---
+++ projets/ToDo/main.js
@@ -0,0 +1,30 @@
+// *************** Variables **************
+const form = document.body.querySelector("form");
+const input = document.body.querySelector("input");
+const list = document.body.querySelector("ul");
+
+// *************** Functions *************
+
+const addThingInToDoList = (value) => {
+  list.innerHTML += `<li>${value}</li>`;
+
+  const li = list.querySelector("li");
+
+  li.addEventListener("click", () => {
+    if (!li.classList.contains("checked")) {
+      li.classList.add("checked");
+    } else {
+      li.remove();
+    }
+  });
+};
+// ***************** Events **************
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addThingInToDoList(input.value);
+  input.value = null;
+});
+
+// ***************** code ****************
