eb9e73e8b40ededc11f8986acb30a9ced18c5a45
Alegoxis <alexisbrossardguillon@gmail.com>
2025-11-04 19:06:52 +0100

    projets v3 je refait l'ui

--- projets/main.js
+++ projets/main.js
@@ -3,10 +3,28 @@ const btn = document.body.querySelector(".toggle-btn");
 const sidebar = document.body.querySelector("#side-bar");
 const content = document.body.querySelector(".content");
 const bubble = document.body.querySelector("#Bubble");
+const list = document.getElementById("project-list");
 
 let toggle = false;
-
+let folderList = {};
+// ********** Function **************
+async function FetchProject() {
+  await fetch("./file_list.json")
+    .then((res) => res.json())
+    .then((data) => (folderList = data));
+  projectsDisplay();
+}
+function projectsDisplay() {
+  for (key in folderList.visible) {
+    list.innerHTML += `
+    <li><i class="fa-solid fa-folder"></i>${key}</li>
+    `;
+  }
+}
 // ********** Events ****************
+window.addEventListener("load", () => {
+  FetchProject();
+});
 btn.addEventListener("click", () => {
   if (toggle === false) {
     sidebar.classList.toggle("active");
@@ -25,4 +43,11 @@ content.addEventListener("click", () => {
 bubble.addEventListener("click", () => {
   location.replace("./bubbles/index.html");
 });
-console.log();
+list.addEventListener("click", (e) => {
+  let carrot = e.target.outerText;
+  console.log(carrot);
+  console.log(folderList.visible[carrot]);
+
+  location.replace(folderList.visible[carrot]);
+});
+// *************** code ********************
